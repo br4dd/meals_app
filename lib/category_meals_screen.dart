0955c4deb31ac51bbc97d78c9b1d216c8724ff7d
br4dd <earljohn321123@gmail.com>
2021-05-21 00:39:40 +0800

    displaying meals on certain category

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   // for safer routes
@@ -9,21 +10,37 @@ class CategoryMealsScreen extends StatelessWidget {
     // getting the arguments of the pushNamed navigator
     // Map<String, String>
     // String keys and String values
+
     final routeArgs =
         ModalRoute.of(context).settings.arguments as Map<String, dynamic>;
+
     // we used Map<String, dynamic>
     // for dynamic values like Color, widgets, etc
+
     final categoryId = routeArgs['id'];
     final categoryTitle = routeArgs['title'];
     final categoryColor = routeArgs['color'];
+
+    // meal as temporary variable
+    // checks if the meal categories contains current categoryId
+
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
         backgroundColor: categoryColor,
       ),
-      body: Center(
-        child: Text(categoryId),
+
+      // for dynamic ListView
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          // to access the meal from different category
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
